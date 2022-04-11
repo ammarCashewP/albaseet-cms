e4c4a902b1ef71e4b9c6186da3fca6a835edbdb0
Ammar Alhasan <87602055+ammarCashewP@users.noreply.github.com>
2022-04-11 22:26:03 +0400

    init

---
+++ config/plugins.js
@@ -0,0 +1,10 @@
+module.exports = ({ env }) => ({
+    upload: {
+        provider: "cloudinary",
+        providerOptions: {
+            cloud_name: "dng7cvpds",
+            api_key: "219344938528617",
+            api_secret: "5uS7cUsgHrlwdFBHAzJcCCQAP5M",
+        },
+    },
+});
