026b9043547f5821fdbb4dd210672ec367e1f018
Ammar Alhasan <87602055+ammarCashewP@users.noreply.github.com>
2022-04-11 23:11:11 +0400

    test

--- config/database.js
+++ config/database.js
@@ -5,12 +5,12 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', 'ec2-44-194-4-127.compute-1.amazonaws.com'),
+        host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'd9juc8fm2rggo3'),
-        username: env('DATABASE_USERNAME', 'amhmbcapcmcjqo'),
-        password: env('DATABASE_PASSWORD', 'bdc26bb10cd9f7079b6819d65fbbaa81436a4af6a77829e7e8225379fbfc18a0'),
-        ssl: env.bool('DATABASE_SSL', true),
+        database: env('DATABASE_NAME', 'albasset'),
+        username: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', 'root'),
+        ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
     },
---
+++ config/env/production/server.js
@@ -0,0 +1,3 @@
+module.exports = ({ env }) => ({
+  url: env('https://albaseet-cms.herokuapp.com'),
+});
